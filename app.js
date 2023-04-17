b399e677674e76422eafbaba1822c4bb05957583
bgordon8 <gordonbrian16@gmail.com>
2023-04-17 17:05:28 -0500

    adds html to server

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My first Page</title></head>");
+  res.write("<body><h1>Hello from the server again!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000, () => {
